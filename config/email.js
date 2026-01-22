10a7dc7fa20d28c40b43ca4a9df96cffd01af595
SurajSingh9696 <surajsingh969670@gmail.com>
2026-01-22 06:06:11 +0530

    Updation

--- config/email.js
+++ config/email.js
@@ -7,7 +7,8 @@ const transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS?.replace(/\s+/g, '') // Remove all spaces from password
+        pass: process.env.EMAIL_PASS,
+        passLoaded: Boolean(process.env.SMTP_PASS)
     },
     tls: {
         rejectUnauthorized: false
