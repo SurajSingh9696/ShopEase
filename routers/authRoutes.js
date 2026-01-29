cdb0f050d165fb1b3185f3d135b5602966808810
SurajSingh9696 <surajsingh969670@gmail.com>
2026-01-29 23:38:50 +0530

    Remove refresh token logic completely - simplified to single token auth

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 
 router.post('/login', require("../middlewares/authMiddleware").login, require('../controllers/authController').login);
 router.post('/register', require("../middlewares/authMiddleware").register, require('../controllers/authController').register);
-router.post('/refresh', require('../controllers/authController').refresh);
 router.post('/logout', (req, res) => {
     // No need to clear cookies anymore, client handles token removal
     return res.status(200).json({ success: true, message: "Logged out successfully" });
